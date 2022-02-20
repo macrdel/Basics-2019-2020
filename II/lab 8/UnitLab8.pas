22dd476a3de09bb54883bfbbd35f324743479f76
macr_del <35114279+macrdel@users.noreply.github.com>
2022-02-20 14:55:47 +0300

    внутренняя сортировка

---
+++ II/lab 8/UnitLab8.pas
@@ -0,0 +1,139 @@
+﻿unit UnitLab8;
+interface
+type
+  TElem = string;
+
+procedure Get(var x: array of TElem; var n: integer; var f: TextFile);
+procedure Put(const x: array of TElem; n: integer; var f: TextFile);
+procedure BubbleSort(var x: array of TElem; n: integer);
+procedure SelectSort(var x: array of TElem; n: integer);
+procedure InsertSort(var x: array of TElem; n: integer);
+procedure QuickSort(var x: array of TElem; n1, n2: integer);
+
+implementation
+
+procedure Get;
+var 
+   i: integer;
+begin
+   readln(f, n);
+   SetLength(x, n);
+   for i:= 0 to n-1 do
+       readln(f, x[i]);  
+end;
+
+procedure Put;
+var
+  i: integer;
+begin
+  for i := 0 to n-1 do
+      write(f, x[i]:4);
+  writeln(f);
+end;
+
+procedure BubbleSort;
+var
+ i, k: integer;
+ y: TElem;
+ f: boolean;
+begin
+ k:= 1;
+    repeat
+     f:= true;
+       for i:= 0 to n - 1 - k do
+        if x[i] > x[i + 1] then
+         begin
+         y:= x[i];
+         x[i]:= x[i + 1];
+         x[i + 1]:= y;
+         f:= false;
+         end;
+      k:= k + 1;
+    until f;
+end;
+
+procedure SelectSort;
+var
+ i, j, imin: integer;
+ min: TElem;
+begin
+   for i:= 0 to n - 2 do
+    begin
+     min:= x[i];
+     imin:= i;
+      for j:= i + 1 to n - 1 do
+       if x[j] < min then
+        begin
+        min:= x[j];
+        imin:= j;
+        end;
+    x[imin]:= x[i];
+    x[i]:= min;
+    end;
+end;
+
+procedure InsertSort;
+var
+ i, j: integer;
+ y: TElem;
+begin
+    for i:= 1 to n - 1 do
+     begin
+     y:= x[i];
+     j:= i - 1;
+      while (j >= 0) and (x[j] > y) do
+       begin
+       x[j + 1]:= x[j];
+       j:= j - 1;
+       end;
+     x[j + 1]:= y;
+     end;
+end;
+
+procedure QuickSort;
+var
+ i, j: integer;
+ y, k: TElem;
+begin
+    if n2 - n1 = 1 then
+    begin
+     if x[n1] > x[n2] then
+     begin
+     y:= x[n1];
+     x[n1]:= x[n2];
+     x[n2]:= y;
+     end;
+    exit;
+    end;
+      k:= x[(n1 + n2) div 2];
+      i:= n1;
+      j:= n2;
+    repeat
+      while (x[i] < k) do 
+        i:= i + 1;
+      while (x[j] > k) do 
+        j:= j - 1;
+         if i < j then
+         begin
+         y:= x[i];
+         x[i]:= x[j];
+         x[j]:= y;
+         i:= i + 1;
+         j:= j - 1;
+         end
+         else
+           if (i = j) then 
+             begin
+          i:= i + 1;
+          j:= j - 1;
+             end;
+    until i > j;
+     if n1 < j then
+      QuickSort(x, n1, j);
+     if i < n2 then
+      QuickSort(x, i, n2);
+end;
+
+end.
+
+
---
+++ II/lab 8/main.pas
@@ -0,0 +1,89 @@
+﻿program Sort;
+uses UnitLab8;
+var
+  a, b: array of TElem;
+  na: integer;
+  fin, fout: TextFile;
+  start, finish: System.DateTime;
+  ts: System.TimeSpan;
+begin
+  if ParamCount < 2 then
+  begin
+    writeln('Недостаточно параметров');
+    exit;
+  end;
+  try
+    AssignFile(fin, ParamStr(1));
+    Reset(fin);
+  except
+    writeln('Невозможно открыть файл ''', Paramstr(1), ''' для чтения');
+    exit;
+  end;
+  try
+    AssignFile(fout, ParamStr(2));
+    Append(fout);
+  except
+    writeln('Невозможно открыть файл ''', Paramstr(2), ''' для записи');
+    exit;
+  end;
+ 
+ Get(a, na, fin);
+ CloseFile(fin);
+ writeln(fout, 'Вид исходного массива из ', na:4, ' элементов : ');
+ writeln(fout);
+ Put(a, na, fout);
+ 
+ SetLength(b, na);
+ b:=a;
+ 
+ writeln(fout, 'Сортировка обменами ');
+ start:= System.DateTime.Now;
+ BubbleSort(a, na);
+ finish:= System.DateTime.Now;
+ ts:= finish - start;
+ writeln(fout, 'Время Сортировка обменами: ', ts.Minutes, ':', ts.Seconds, '.', ts.Milliseconds);
+ writeln(fout, 'Отсортированный Массив метод Сортировки Обменами: ');
+ writeln(fout);
+ Put(a, na, fout);
+ writeln(fout);
+ 
+ writeln(fout, 'Сортировка выбором ');
+ a:=b;
+ start:= System.DateTime.Now;
+ SelectSort(a, na);
+ finish:= System.DateTime.Now;
+ ts:= finish - start;
+ writeln(fout, 'Время Сортировка выбором: ', ts.Minutes, ':', ts.Seconds, '.', ts.Milliseconds);
+ writeln(fout, 'Отсортированный Массив метод Сортировки выбором: ');
+ writeln(fout);
+ Put(a, na, fout);
+ writeln(fout);
+ 
+ writeln(fout, 'Сортировка вставками ');
+ a:=b;
+ start:= System.DateTime.Now;
+ InsertSort(a, na);
+ finish:= System.DateTime.Now;
+ ts:= finish - start;
+ writeln(fout, 'Время Сортировка вставками: ', ts.Minutes, ':', ts.Seconds, '.', ts.Milliseconds);
+ writeln(fout, 'Отсортированный Массив метод Сортировки вставками: ');
+ writeln(fout);
+ Put(a, na, fout);
+ writeln(fout);
+ 
+ writeln(fout, 'Быстрая Сортировка ');
+ a:=b;
+ start:= System.DateTime.Now;
+ SelectSort(a, na);
+ finish:= System.DateTime.Now;
+ ts:= finish - start;
+ writeln(fout, 'Время Быстрая Сортировка ', ts.Minutes, ':', ts.Seconds, '.', ts.Milliseconds);
+ writeln(fout, 'Отсортированный Массив метод Быстрой Сортировки: ');
+ writeln(fout);
+ Put(a, na, fout);
+ writeln(fout);
+ 
+ Writeln(fout);
+ CloseFile(fout);
+ 
+end.  
